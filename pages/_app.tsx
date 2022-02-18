a09b9f0897a4ce8c5ccbb822c1f54d2625f4d656
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2022-02-18 20:14:13 +0300

    Document Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import { AppProps } from 'next/dist/shared/lib/router/router';
 import '../styles/globals.css';
+import Head from 'next/head';
+import React from 'react';
+import { AppProps } from 'next/dist/shared/lib/router/router';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
