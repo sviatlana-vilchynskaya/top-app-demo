f386fe0c25dc23c7d1cf3fc25143dee3284f2f32
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2022-02-20 15:08:36 +0300

    @svgr/webpack

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,9 +1,11 @@
 import { ButtonProps } from './Button.props';
 import styles from './Button.module.css';
 import cn from 'classnames';
+import ArrowIcon from './arrow.svg';
 
 export const Button = ({
   appearance,
+  arrow = 'none',
   children,
   className,
   ...props
@@ -17,6 +19,15 @@ export const Button = ({
       {...props}
     >
       {children}
+      {arrow != 'none' && (
+        <span
+          className={cn(styles.arrow, {
+            [styles.down]: arrow == 'down',
+          })}
+        >
+          <ArrowIcon />
+        </span>
+      )}
     </button>
   );
 };
