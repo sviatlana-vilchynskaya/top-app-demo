2f49a3f0e6e7283f70d6020a295353d5f9fce033
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2022-03-17 18:10:46 +0300

    Component P, fix svg, next-env-custom

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
 module.exports = {
-  webpack(config) {
+  images: {
+    domains: ['courses-top.ru'],
+  },
+  webpack(config, options) {
     config.module.rules.push({
+      loader: '@svgr/webpack',
+      issuer: /\.[jt]sx?$/,
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                override: {
+                  removeViewBox: false,
+                },
+              },
+            },
+          ],
+        },
+        titleProp: true,
+      },
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
     });
 
     return config;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Htag } from '../components';
+import { Button, Htag, P } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -11,6 +11,13 @@ export default function Home(): JSX.Element {
       <Button appearance="ghost" arrow="down">
         Кнопка
       </Button>
+      <P size="l">
+        {' '}
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque
+        provident a quo nemo vero! Quasi expedita odio aut numquam! Est
+        praesentium autem magnam fugiat eligendi quisquam consequatur aperiam,
+        quia laborum?
+      </P>
     </>
   );
 }
